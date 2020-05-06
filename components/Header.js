5c1b947e788f67b1af5a10596bfb72f17142a1bb
Riley Seaburg <riley@rds.marketing>
2020-05-05 20:40:22 -0400

    end of day push

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,28 @@
 import React from "react";
 import styled from "@emotion/styled";
-import {rem} from 'polished'
+import { rem } from "polished";
+import { Flex, Box } from "reflexbox";
+import Navigation from "../components/Navigation";
+import Link from "next/link";
+
 function Header({ isDark }) {
   return (
     <HeaderStyled isDark={isDark}>
-      <div className="container">
-        <div className="container logo">
-          <a href="/">
-            <img src="/public/vercel.svg" alt="" />
-          </a>
-          <span className="logo-text">Company Name Here</span>
-        </div>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
+      <Box variant="container">
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          className="container logo"
+        >
+          <Link href="/">
+            <a>
+              <img src="/public/vercel.svg" alt="" />
+              <span className="logo-text">Company Name Here</span>
+            </a>
+          </Link>
+          <Navigation />
+        </Flex>
+      </Box>
     </HeaderStyled>
   );
 }
@@ -45,11 +42,11 @@ const HeaderStyled = styled.header`
     }
   }
   .logo {
-      display: flex;
-      color: #333;
-      font-weight: bold;
-      font-size: ${rem(20)};
-      margin-left: ${rem(2)}
+    display: flex;
+    color: #333;
+    font-weight: bold;
+    font-size: ${rem(20)};
+    margin-left: ${rem(2)};
   }
 `;
 
